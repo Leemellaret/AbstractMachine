c2171d2bd0f7e6e90595bc838dcb176d87312bfb
leemellaret <leemellaret@gmail.com>
2021-03-28 00:38:15 +0500

    Add DomainOperationsExtensions

--- AbstractMachine/Program.cs
+++ AbstractMachine/Program.cs
@@ -16,14 +16,20 @@ namespace AbstractMachine
                                                                                         .AddRow((0, 0), (0, 0))
                                                                                         .AddRow((0, 1), (1, 0))
                                                                                         .AddRow((1, 0), (1, 1))
-                                                                                        .AddRow((1, 1), (0, 1))
                                                                                         .SetInitialState(1);
 
             var abstractMachine = builder.CreateInstance();
             var inputDomain = builder.GetInputDomainInstance();
 
-            Console.WriteLine($"a(0) = {abstractMachine.Process(new Information<Integer>(1, inputDomain))}");
+            Console.WriteLine($"a(0) = {abstractMachine.Process(new Information<Integer>(0, inputDomain))}");
             Console.WriteLine($"a.CS = {abstractMachine.CurrentState}");
+
+            //.AddRow((1, 1), (0, 1))
+
+            var am2 = new MappingAbstractMachineBuilder<Integer, Integer, Integer>(abstractMachine).AddRow((1, 1), (0, 1)).CreateInstance();
+
+            Console.WriteLine($"a(0) = {am2.Process(new Information<Integer>(1, inputDomain))}");
+            Console.WriteLine($"a.CS = {am2.CurrentState}");
             Console.ReadLine();
         }
     }
